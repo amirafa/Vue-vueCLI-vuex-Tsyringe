5e3c44f957c8e9b044bf803219358ec2550d39ba
AmirHossein <amir.h.rafati@gmail.com>
2021-12-17 21:23:50 +0330

    new

--- src/pages/Marketing.vue
+++ src/pages/Marketing.vue
@@ -1,49 +1,61 @@
 <template>
-  <div></div>
+    <div v-if="!loading" class="container">
+    <div class="row d-flex justify-content-center align-items-center vh-100">
+      <div
+        class="spinner-grow"
+        style="width: 3rem; height: 3rem"
+        role="status"
+      ></div>
+    </div>
+  </div>
+
+  <div v-else class="container">
+    <div class="row">
+      <header class="p-2 m-0">
+        <h1>{{ title }}</h1>
+      </header>
+      <div
+        class="d-inline-flex col-12 col-md-4"
+        v-for="(plugin, index) in tabPluginsArr"
+        :key="index"
+      >
+        <Plugin
+          :plugin="plugin"
+          :plugin-status="pluginStatus"
+          :is-toggled="isToggled"
+          :tab-data="tabData"
+          :tab-number="tab"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Plugin from "../components/Plugin.vue";
+import { mixins } from 'vue-class-component'
 
-@Component({
-  components: {
-    Plugin,
-  },
-})
-export default class Marketing extends Vue {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+
+export default class Marketing extends mixins {
+  tab: number|undefined;
+  
   data() {
-    return {};
+    return {
+        tab:1,
+    };
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  async mounted() {
-    console.log("App mounted");
+  mounted() {
+    console.log("Tab_Page mounted");
+    this.checkRoute
   }
-  // // @Prop()
-  // // private msg!: string;
-  // msg = "";
-  // name = "";
 
-  // data() {
-  //   return {
-  //     msg: "hello",
-  //     nme: "amir",
-  //   };
-  // }
-  // // mounted() {
-  // //   this.name="amir"
-  // // }
+  checkRoute() {
+      if (this.$route.name == "merkating") this.tab = 1;
+      else if (this.$route.name == "finance") this.tab = 2;
+      else if (this.$route.name == "personnel") this.tab = 3;
+    }
 
-  // doSth(): void {
-  //   this.msg = "bye";
-  //   this.name = "ali";
-  // }
-
-  // show(a) {
-  //   this.msg += ` ${a.last}`;
-  // }
 }
 </script>
 
