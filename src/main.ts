35ceffc8ce2d1679ded4622e4514e30ff23257bf
amirhrafati <amir.h.rafati@gmail.com>
2021-12-18 16:23:52 +0330

    vuex added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from '.'
+import router from './router'
+import store from './store'
 import './class-component-hooks'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
