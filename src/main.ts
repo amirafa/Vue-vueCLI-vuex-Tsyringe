ecbc9337525d3e68137f41856990ad97c875641f
amirhrafati <amir.h.rafati@gmail.com>
2021-12-18 16:39:23 +0330

    new

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './class-component-hooks'
+// Bootstrap
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
