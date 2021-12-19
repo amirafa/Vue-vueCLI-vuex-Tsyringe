2fc97b7dc0ea8a9d12ecb054a1d86b0812a262d2
amirhrafati <amir.h.rafati@gmail.com>
2021-12-19 09:40:49 +0330

    new

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,57 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loadingStatus: false,
+    data: {},
+    getUrl: "https://run.mocky.io/v3/c18c464e-6771-4de2-8d09-603c09624130",
+    postUrl: "https://run.mocky.io/v3/c18c464e-6771-4de2-8d09-603c09624130",
   },
   mutations: {
+    SET_LADING_STATUS(state, status) {
+      state.loadingStatus = status;
+    },
+    SET_DATA(state, data) {
+      state.data = data;
+    },
   },
   actions: {
+    fetchData(context) {
+      context.commit("SET_LOADING_STATUS", true);
+      axios
+        //.get(`http://localhost:3000/data`)
+        .get(this.state.getUrl)
+        .then(function (response: any) {
+          context.commit("SET_LOADING_STATUS", false);
+          context.commit("SET_DATA", response.data);
+        });
+    },
+    postData(context, post) {
+      context.commit("SET_LOADING_STATUS", true);
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      };
+      axios
+        .post(
+          this.state.postUrl, //"http://localhost:3000/data",
+          post,
+          { headers }
+        )
+        .then(function (response) {
+          console.log("Post Response ", response);
+        });
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    getData(state){
+      return state.data;
+    }
+  },
+  modules: {},
+});
