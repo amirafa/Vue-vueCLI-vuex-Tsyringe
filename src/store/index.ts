511dbc90b15eab5d5c937b7e98bbaf4f27c95fe6
amirhrafati <amir.h.rafati@gmail.com>
2021-12-19 10:57:41 +0330

    mixins added

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,7 @@ export default new Vuex.Store({
   state: {
     loadingStatus: false,
     data: {},
-    getUrl: "https://run.mocky.io/v3/c18c464e-6771-4de2-8d09-603c09624130",//.get(`http://localhost:3000/data`)
+    getUrl: "https://run.mocky.io/v3/c18c464e-6771-4de2-8d09-603c09624130", //.get(`http://localhost:3000/data`)
     postUrl: "https://run.mocky.io/v3/c18c464e-6771-4de2-8d09-603c09624130",
   },
   mutations: {
@@ -32,46 +32,70 @@ export default new Vuex.Store({
     //     });
     // },
     fetchData(context) {
-      return new Promise((resolve,reject)=>{
-        console.log("context",context)
+      return new Promise((resolve, reject) => {
+        console.log("context", context);
         context.commit("SET_LOADING_STATUS", true);
         axios
           //.get(`http://localhost:3000/data`)
           .get(this.state.getUrl)
-          .then( (response: any) => {
+          .then((response: any) => {
             context.commit("SET_LOADING_STATUS", false);
             context.commit("SET_DATA", response.data);
-            resolve(this.state.loadingStatus)
-          }).catch((err)=>{
-            reject(err)
+            resolve(this.state.loadingStatus);
           })
-      })
+          .catch((err) => {
+            reject(err);
+          });
+      });
     },
+    // postData(context, post) {
+    //   context.commit("SET_LOADING_STATUS", true);
+    //   const headers = {
+    //     "Access-Control-Allow-Origin": "*",
+    //     Accept: "application/json, text/plain, */*",
+    //     "Content-Type": "application/json",
+    //   };
+    //   axios
+    //     .post(
+    //       this.state.postUrl, //"http://localhost:3000/data",
+    //       post,
+    //       { headers }
+    //     )
+    //     .then(function (response) {
+    //       console.log("Post Response ", response);
+    //     });
+    // },
     postData(context, post) {
-      context.commit("SET_LOADING_STATUS", true);
-      const headers = {
-        "Access-Control-Allow-Origin": "*",
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      };
-      axios
-        .post(
-          this.state.postUrl, //"http://localhost:3000/data",
-          post,
-          { headers }
-        )
-        .then(function (response) {
-          console.log("Post Response ", response);
-        });
+      return new Promise((resolve, reject) => {
+        context.commit("SET_LOADING_STATUS", true);
+        const headers = {
+          "Access-Control-Allow-Origin": "*",
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        };
+        axios
+          .post(
+            this.state.postUrl, //"http://localhost:3000/data",
+            post,
+            { headers }
+          )
+          .then((response) => {
+            console.log("Post Response ", response);
+            resolve(this.state.loadingStatus);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
     },
   },
   getters: {
-    getData(state){
+    getData(state) {
       return state.data;
     },
-    getLoadingStatus(state){
-      return state.loadingStatus
-    }
+    getLoadingStatus(state) {
+      return state.loadingStatus;
+    },
   },
   modules: {},
 });
