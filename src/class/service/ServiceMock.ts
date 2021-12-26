306ae07a47183c87371f7303a219001b561cc198
amirhrafati <amir.h.rafati@gmail.com>
2021-12-26 14:59:24 +0330

    ServiceMock

---
+++ src/class/service/ServiceMock.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+import store from '@/store/index'
+import { Myjson } from "@/interface/interface";
+
+export class Service {
+  data: string;
+  getUrl:string;
+  postUrl:string;
+
+  constructor() {
+    this.data = "";
+    this.getUrl=process.env.VUE_APP_API_MOCK
+    this.postUrl=process.env.VUE_APP_API_MOCK
+  }
+
+  getData(): Promise<Myjson> {
+    store.dispatch("setLoadingStatus",true)
+    return new Promise((resolve:any, reject:any) => {
+      axios
+        //.get(`http://localhost:3000/data`)
+        .get(this.getUrl)
+        .then(function (response) {
+          console.log(response);
+          //console.log("Get : ", response.data);
+          store.dispatch('setData',response.data)
+          store.dispatch("setLoadingStatus",false)
+          resolve(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+          reject(error);
+        });
+    });
+  }
+
+  postData(post: Myjson):Promise<string>  {
+    //store.dispatch("setLoadingStatus",true)
+    return new Promise((resolve:any, reject:any) => {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      };
+      axios
+        .post(
+          this.postUrl,
+          //"http://localhost:3000/data",
+          post,
+          { headers }
+        )
+        .then((response) => {
+          console.log("Server Response ", response);
+
+          //store.dispatch("setLoadingStatus",false)
+          store.dispatch("setData",post)
+          
+          resolve(response);
+        })
+        .catch(function (error) {
+          store.dispatch("setLoadingStatus",false)
+          console.log("postData ",error)
+          reject(error);
+        });
+    });
+  }
+
+  set myData(params: string) {
+    this.data = params;
+    //console.log("data Setter");
+  }
+
+  get myData(): string {
+    //console.log("data Getter");
+    return this.data;
+  }
+}
